f64bcf3ceb0df97697df39eec59f415bf8b24c5b
jordanYue116 <jordantyue@gmail.com>
2026-07-09 18:37:02 -0700

    Added Github hosting stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jordanYue116.github.io",
+  base: "/devportfolio/",
   vite: {
     plugins: [tailwindcss()],
   },
